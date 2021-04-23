37127a16718e376294fd33c3658397c8de5f0ec0
Ahmed Sarhan <ahmedsarhan1515@gmail.com>
2021-04-24 00:37:46 +0200

    useForm options

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useForm } from 'react-hook-form';
 import axios from 'axios';
-import { defaultValue, defaultValues } from '../utils/helpers';
+import { defaultValues } from '../utils/helpers';
 const ContactForm = () => {
 	const {
 		register,
